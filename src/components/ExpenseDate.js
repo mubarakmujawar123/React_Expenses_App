aa5d886b5d4a1c314321f01fe8c9e42ceeba56e4
mubarakmujawar123 <mubarak.mujawar123@gmail.com>
2021-10-15 11:28:34 +0530

    2 files modified, 2 added

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ExpenseDate.css'
+import './ExpenseDate.css';
 
 function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'short' });
@@ -10,7 +10,7 @@ function ExpenseDate(props) {
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
-      <div className="expense-date__date">{day}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
