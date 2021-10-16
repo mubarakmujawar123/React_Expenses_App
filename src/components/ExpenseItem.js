835b2018c883596b3e10a747e6f30c64a59c18f4
mubarakmujawar123 <mubarak.mujawar123@gmail.com>
2021-10-16 09:59:39 +0530

    1 file modified

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
+  const buttonClickHandler = () => {
+    console.log('clicked');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,7 +13,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={buttonClickHandler}> chane title</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
